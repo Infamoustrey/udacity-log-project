a6155a9a448ed447e8e46097f915030761469751
Trey Foster <treybfoster@gmail.com>
2018-11-23 10:58:50 -0600

    Revise Query and README

--- README.md
+++ README.md
@@ -11,6 +11,48 @@ the following statistics:
 
 It prints the results to the console via the standard output. 
 
+## Prerequisites
+
+You will need vagrant, an editor, and a terminal for this project.
+
+## Setup
+
+This program analyses the news db for the following vagrant vm.
+To setup clone the repository:
+
+```bash 
+git clone https://github.com/udacity/fullstack-nanodegree-vm.git
+```
+
+Then cd into the vagrant directory and launch the vm
+
+```bash
+cd fullstack-nanodegree-vm.git/vagrant
+vagrant up 
+```
+
+Then download the `newsdata.sql` file from [this](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip) link.
+
+Copy the file into the `vagrant` directory on your local machine and run the following command to setup the db:
+
+```bash 
+psql -d news -f newsdata.sql
+```
+
+Now you'll need to clone this program repo into the `vagrant` dir on your local machine, to do so run the following command:
+
+```bash
+git clone https://github.com/Infamoustrey/udacity-log-project.git
+```
+
+Now you'll want to ssh into the vagrant machine and get into the `vagrant` dir to be able to run the report. From your localmachine run the following:
+
+```bash
+vagrant ssh
+cd /vagrant/udacity-log-project
+python3 log_report.py
+```
+
 ## Usage 
 To run simply run the following: 
 
--- log_report.py
+++ log_report.py
@@ -6,12 +6,11 @@ import psycopg2
 most_popular_articles = """
     select
         '"' || title || '"',
-        (select count(id)
-            from log
-            where log.path
-            like '%' || articles.slug || '%'
-        ) as views
+        count(log.id) as views
     from articles
+    join log on log.path like '%' || articles.slug
+    where log.status = '200 OK'
+    group by title
     order by views desc
     limit 3
 """
@@ -19,10 +18,11 @@ most_popular_articles = """
 most_popular_authors = """
     select
         name,
-        (select count(log.id)
-        from articles, log
-        where log.path like '%' || articles.slug || '%') as views
+        count(log.id) as views
     from authors
+    join articles on articles.author = authors.id
+    join log on log.path like '%' || articles.slug
+    group by name
     order by views desc
     limit 3
 """
@@ -35,15 +35,10 @@ days_errors_above_one_percent = """
             /
             count(id) * 100
             , 1
-        )
+        ) AS percentage
     from log
     group by time::date
-    having round(
-        count(case when status <> '200 OK' then 1 end)::decimal
-        /
-        count(id) * 100
-        , 1
-        ) >= 1
+    having percentage >= 1
 """
 
 
@@ -70,12 +65,12 @@ print_results(query_db(most_popular_articles))
 
 print('\n')
 
-# Print Most Popular Authors
+# # Print Most Popular Authors
 print("Most Popular Authors: ")
 print_results(query_db(most_popular_authors))
 
 print('\n')
 
-# Print Days Where Errors Exceeded 1%
+# # # Print Days Where Errors Exceeded 1%
 print("Days where errors exceeded 1% of requests: ")
 print_results(query_db(days_errors_above_one_percent))
